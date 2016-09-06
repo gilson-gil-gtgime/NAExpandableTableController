fbd737849d20f761d3988bd47ce96652ae16e9d9
Gilson Gil <gilson.gil@me.com>
2016-09-06 12:14:50 -0300

    forward all delegate methods

--- Pod/Classes/NAExpandableTableController.swift
+++ Pod/Classes/NAExpandableTableController.swift
@@ -27,10 +27,20 @@ import UIKit
     
     /// The height of the expandable section title cell
     optional func expandableTableView(tableView: UITableView, heightForTitleCellInSection section: Int) -> CGFloat
+  
+    optional func expandableTableView(tableView: UITableView, titleForFooterInSection section: Int) -> String?
+  
+    optional func expandableTableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool
+  
+    optional func expandableTableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool
+  
+    optional func expandableTableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath)
+  
+    optional func expandableTableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath)
 }
 
 @objc public protocol NAExpandableTableViewDelegate {
-    
+  
     /// Equivalent to UITableView didSelectRowAtIndexPath delegate method. Called whenever a cell within a section is selected. This is NOT called when a section title cell is selected.
     optional func expandableTableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
     
@@ -43,19 +53,110 @@ import UIKit
      - Parameter expanded: True if section is being expanded, false if being collapsed
      */
     optional func expandableTableView(tableView: UITableView, didExpandSection section: Int, expanded: Bool)
+  
+    optional func expandableTableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath)
+  
+    optional func expandableTableView(tableView: UITableView, willDisplayFooterView view: UIView, forSection section: Int)
+  
+    optional func expandableTableView(tableView: UITableView, didEndDisplayingCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath)
+  
+    optional func expandableTableView(tableView: UITableView, didEndDisplayingFooterView view: UIView, forSection section: Int)
+  
+    optional func expandableTableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat
+  
+    optional func expandableTableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat
+  
+    optional func expandableTableView(tableView: UITableView, estimatedHeightForFooterInSection section: Int) -> CGFloat
+  
+    optional func expandableTableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView?
+  
+    optional func expandableTableView(tableView: UITableView, accessoryButtonTappedForRowWithIndexPath indexPath: NSIndexPath)
+  
+    optional func expandableTableView(tableView: UITableView, shouldHighlightRowAtIndexPath indexPath: NSIndexPath) -> Bool
+  
+    optional func expandableTableView(tableView: UITableView, didHighlightRowAtIndexPath indexPath: NSIndexPath)
+  
+    optional func expandableTableView(tableView: UITableView, didUnhighlightRowAtIndexPath indexPath: NSIndexPath)
+  
+    optional func expandableTableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath?
+  
+    optional func expandableTableView(tableView: UITableView, willDeselectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath?
+  
+    optional func expandableTableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath)
+  
+    optional func expandableTableView(tableView: UITableView, editingStyleForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCellEditingStyle
+  
+    optional func expandableTableView(tableView: UITableView, titleForDeleteConfirmationButtonForRowAtIndexPath indexPath: NSIndexPath) -> String?
+  
+    optional func expandableTableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [UITableViewRowAction]?
+  
+    optional func expandableTableView(tableView: UITableView, shouldIndentWhileEditingRowAtIndexPath indexPath: NSIndexPath) -> Bool
+  
+    optional func expandableTableView(tableView: UITableView, willBeginEditingRowAtIndexPath indexPath: NSIndexPath)
+  
+    optional func expandableTableView(tableView: UITableView, didEndEditingRowAtIndexPath indexPath: NSIndexPath)
+  
+    optional func expandableTableView(tableView: UITableView, targetIndexPathForMoveFromRowAtIndexPath sourceIndexPath: NSIndexPath, toProposedIndexPath proposedDestinationIndexPath: NSIndexPath) -> NSIndexPath
+  
+    optional func expandableTableView(tableView: UITableView, indentationLevelForRowAtIndexPath indexPath: NSIndexPath) -> Int
+  
+    optional func expandableTableView(tableView: UITableView, shouldShowMenuForRowAtIndexPath indexPath: NSIndexPath) -> Bool
+  
+    optional func expandableTableView(tableView: UITableView, canPerformAction action: Selector, forRowAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool
+  
+    optional func expandableTableView(tableView: UITableView, performAction action: Selector, forRowAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?)
+  
+    optional func expandableTableView(tableView: UITableView, canFocusRowAtIndexPath indexPath: NSIndexPath) -> Bool
+  
+    @available(iOS 9.0, *)
+    optional func expandableTableView(tableView: UITableView, shouldUpdateFocusInContext context: UITableViewFocusUpdateContext) -> Bool
+  
+    @available(iOS 9.0, *)
+    optional func expandableTableView(tableView: UITableView, didUpdateFocusInContext context: UITableViewFocusUpdateContext, withAnimationCoordinator coordinator: UIFocusAnimationCoordinator)
+  
+    optional func indexPathForPreferredFocusedViewInExpandableTableView(tableView: UITableView) -> NSIndexPath?
 }
 
-public class NAExpandableTableController: NSObject, UITableViewDataSource, UITableViewDelegate {
-    
+@objc public protocol NAExpandableScrollViewDelegate {
+  optional func expandableScrollViewDidScroll(scrollView: UIScrollView)
+  
+  optional func expandableScrollViewDidZoom(scrollView: UIScrollView)
+  
+  optional func expandableScrollViewWillBeginDragging(scrollView: UIScrollView)
+  
+  optional func expandableScrollViewWillEndDragging(scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>)
+  
+  optional func expandableScrollViewDidEndDragging(scrollView: UIScrollView, willDecelerate decelerate: Bool)
+  
+  optional func expandableScrollViewWillBeginDecelerating(scrollView: UIScrollView)
+  
+  optional func expandableScrollViewDidEndDecelerating(scrollView: UIScrollView)
+  
+  optional func expandableScrollViewDidEndScrollingAnimation(scrollView: UIScrollView)
+  
+  optional func viewForZoomingInExpandableScrollView(scrollView: UIScrollView) -> UIView?
+  
+  optional func expandableScrollViewWillBeginZooming(scrollView: UIScrollView, withView view: UIView?)
+  
+  optional func expandableScrollViewDidEndZooming(scrollView: UIScrollView, withView view: UIView?, atScale scale: CGFloat)
+  
+  optional func expandableScrollViewShouldScrollToTop(scrollView: UIScrollView) -> Bool
+  
+  optional func expandableScrollViewDidScrollToTop(scrollView: UIScrollView)
+}
+
+public class NAExpandableTableController: NSObject, UITableViewDataSource, UITableViewDelegate, UIScrollViewDelegate {
+  
     /// Default height to use for all cells (44)
     public var defaultRowHeight: CGFloat = 44
-    
+  
     /// Determines if multiple sections can be expanded at the same time. If set to `true`, then only one section can be expanded at a time. If a section is expanded and you try to expand another section, the first one will be collapsed.
     public var exclusiveExpand: Bool = false
     
     public weak var dataSource: NAExpandableTableViewDataSource?
     public weak var delegate: NAExpandableTableViewDelegate?
-    
+    public weak var scrollViewDelegate: NAExpandableScrollViewDelegate?
+  
     /// Keeps track of which section indices are expanded
     private var expandDict = [Int: Bool]()
     
@@ -168,4 +269,207 @@ public class NAExpandableTableController: NSObject, UITableViewDataSource, UITab
         
         delegate?.expandableTableView?(tableView, didExpandSection: section, expanded: false)
     }
+}
+
+// MARK: Other UITableView DataSource methods default implementation
+extension NAExpandableTableController {
+  public func tableView(tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+    return dataSource?.expandableTableView?(tableView, titleForFooterInSection: section)
+  }
+  
+  public func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    return dataSource?.expandableTableView?(tableView, canEditRowAtIndexPath: indexPath) ?? false
+  }
+  
+  public func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    return dataSource?.expandableTableView?(tableView, canMoveRowAtIndexPath: indexPath) ?? false
+  }
+  
+  public func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+    dataSource?.expandableTableView?(tableView, commitEditingStyle: editingStyle, forRowAtIndexPath: indexPath)
+  }
+  
+  public func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
+    dataSource?.expandableTableView?(tableView, moveRowAtIndexPath: sourceIndexPath, toIndexPath: destinationIndexPath)
+  }
+}
+
+// MARK: Other UITableView DataSource methods default implementation
+extension NAExpandableTableController {
+  public func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+    delegate?.expandableTableView?(tableView, willDisplayCell: cell, forRowAtIndexPath: indexPath)
+  }
+  
+  public func tableView(tableView: UITableView, willDisplayFooterView view: UIView, forSection section: Int) {
+    delegate?.expandableTableView?(tableView, willDisplayFooterView: view, forSection: section)
+  }
+  
+  public func tableView(tableView: UITableView, didEndDisplayingCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+    delegate?.expandableTableView?(tableView, didEndDisplayingCell: cell, forRowAtIndexPath: indexPath)
+  }
+  
+  public func tableView(tableView: UITableView, didEndDisplayingFooterView view: UIView, forSection section: Int) {
+    delegate?.expandableTableView?(tableView, didEndDisplayingFooterView: view, forSection: section)
+  }
+  
+  public func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+    return delegate?.expandableTableView?(tableView, heightForFooterInSection: section) ?? 0
+  }
+  
+  public func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+    return delegate?.expandableTableView?(tableView, estimatedHeightForRowAtIndexPath: indexPath) ?? defaultRowHeight
+  }
+  
+  public func tableView(tableView: UITableView, estimatedHeightForFooterInSection section: Int) -> CGFloat {
+    return delegate?.expandableTableView?(tableView, estimatedHeightForFooterInSection: section) ?? 0
+  }
+  
+  public func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+    return delegate?.expandableTableView?(tableView, viewForFooterInSection: section)
+  }
+  
+  public func tableView(tableView: UITableView, accessoryButtonTappedForRowWithIndexPath indexPath: NSIndexPath) {
+    delegate?.expandableTableView?(tableView, accessoryButtonTappedForRowWithIndexPath: indexPath)
+  }
+  
+  public func tableView(tableView: UITableView, shouldHighlightRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    return delegate?.expandableTableView?(tableView, shouldHighlightRowAtIndexPath: indexPath) ?? false
+  }
+  
+  public func tableView(tableView: UITableView, didHighlightRowAtIndexPath indexPath: NSIndexPath) {
+    delegate?.expandableTableView?(tableView, didHighlightRowAtIndexPath: indexPath)
+  }
+  
+  public func tableView(tableView: UITableView, didUnhighlightRowAtIndexPath indexPath: NSIndexPath) {
+    delegate?.expandableTableView?(tableView, didUnhighlightRowAtIndexPath: indexPath)
+  }
+  
+  public func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+    return delegate?.expandableTableView?(tableView, willSelectRowAtIndexPath: indexPath)
+  }
+  
+  public func tableView(tableView: UITableView, willDeselectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+    return delegate?.expandableTableView?(tableView, willDeselectRowAtIndexPath: indexPath)
+  }
+  
+  public func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+    delegate?.expandableTableView?(tableView, didDeselectRowAtIndexPath: indexPath)
+  }
+  
+  public func tableView(tableView: UITableView, editingStyleForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCellEditingStyle {
+    return delegate?.expandableTableView?(tableView, editingStyleForRowAtIndexPath: indexPath) ?? .None
+  }
+  
+  public func tableView(tableView: UITableView, titleForDeleteConfirmationButtonForRowAtIndexPath indexPath: NSIndexPath) -> String? {
+    return delegate?.expandableTableView?(tableView, titleForDeleteConfirmationButtonForRowAtIndexPath: indexPath)
+  }
+  
+  public func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [UITableViewRowAction]? {
+    return delegate?.expandableTableView?(tableView, editActionsForRowAtIndexPath: indexPath)
+  }
+  
+  public func tableView(tableView: UITableView, shouldIndentWhileEditingRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    return delegate?.expandableTableView?(tableView, shouldIndentWhileEditingRowAtIndexPath: indexPath) ?? false
+  }
+  
+  public func tableView(tableView: UITableView, willBeginEditingRowAtIndexPath indexPath: NSIndexPath) {
+    delegate?.expandableTableView?(tableView, willBeginEditingRowAtIndexPath: indexPath)
+  }
+  
+  public func tableView(tableView: UITableView, didEndEditingRowAtIndexPath indexPath: NSIndexPath) {
+    delegate?.expandableTableView?(tableView, didEndEditingRowAtIndexPath: indexPath)
+  }
+  
+  public func tableView(tableView: UITableView, targetIndexPathForMoveFromRowAtIndexPath sourceIndexPath: NSIndexPath, toProposedIndexPath proposedDestinationIndexPath: NSIndexPath) -> NSIndexPath {
+    return delegate?.expandableTableView?(tableView, targetIndexPathForMoveFromRowAtIndexPath: sourceIndexPath, toProposedIndexPath: proposedDestinationIndexPath) ?? sourceIndexPath
+  }
+  
+  public func tableView(tableView: UITableView, indentationLevelForRowAtIndexPath indexPath: NSIndexPath) -> Int {
+    return delegate?.expandableTableView?(tableView, indentationLevelForRowAtIndexPath: indexPath) ?? 0
+  }
+  
+  public func tableView(tableView: UITableView, shouldShowMenuForRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    return delegate?.expandableTableView?(tableView, shouldShowMenuForRowAtIndexPath: indexPath) ?? false
+  }
+  
+  public func tableView(tableView: UITableView, canPerformAction action: Selector, forRowAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool {
+    return delegate?.expandableTableView?(tableView, canPerformAction: action, forRowAtIndexPath: indexPath, withSender: sender) ?? false
+  }
+  
+  public func tableView(tableView: UITableView, performAction action: Selector, forRowAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) {
+    delegate?.expandableTableView?(tableView, performAction: action, forRowAtIndexPath: indexPath, withSender: sender)
+  }
+  
+  public func tableView(tableView: UITableView, canFocusRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    return delegate?.expandableTableView?(tableView, canFocusRowAtIndexPath: indexPath) ?? false
+  }
+  
+  @available(iOS 9.0, *)
+  public func tableView(tableView: UITableView, shouldUpdateFocusInContext context: UITableViewFocusUpdateContext) -> Bool {
+    return delegate?.expandableTableView?(tableView, shouldUpdateFocusInContext: context) ?? false
+  }
+  
+  @available(iOS 9.0, *)
+  public func tableView(tableView: UITableView, didUpdateFocusInContext context: UITableViewFocusUpdateContext, withAnimationCoordinator coordinator: UIFocusAnimationCoordinator) {
+    delegate?.expandableTableView?(tableView, didUpdateFocusInContext: context, withAnimationCoordinator: coordinator)
+  }
+  
+  public func indexPathForPreferredFocusedViewInTableView(tableView: UITableView) -> NSIndexPath? {
+    return delegate?.indexPathForPreferredFocusedViewInExpandableTableView?(tableView)
+  }
+}
+
+// MARK: Other UITableView DataSource methods default implementation
+extension NAExpandableTableController {
+  public func scrollViewDidScroll(scrollView: UIScrollView) {
+    scrollViewDelegate?.expandableScrollViewDidScroll?(scrollView)
+  }
+  
+  public func scrollViewDidZoom(scrollView: UIScrollView) {
+    scrollViewDelegate?.expandableScrollViewDidZoom?(scrollView)
+  }
+  
+  public func scrollViewWillBeginDragging(scrollView: UIScrollView) {
+    scrollViewDelegate?.expandableScrollViewWillBeginDragging?(scrollView)
+  }
+  
+  public func scrollViewWillEndDragging(scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+    scrollViewDelegate?.expandableScrollViewWillEndDragging?(scrollView, withVelocity: velocity, targetContentOffset: targetContentOffset)
+  }
+  
+  public func scrollViewDidEndDragging(scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+    scrollViewDelegate?.expandableScrollViewDidEndDragging?(scrollView, willDecelerate: decelerate)
+  }
+  
+  public func scrollViewWillBeginDecelerating(scrollView: UIScrollView) {
+    scrollViewDelegate?.expandableScrollViewWillBeginDecelerating?(scrollView)
+  }
+  
+  public func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+    scrollViewDelegate?.expandableScrollViewDidEndDecelerating?(scrollView)
+  }
+  
+  public func scrollViewDidEndScrollingAnimation(scrollView: UIScrollView) {
+    scrollViewDelegate?.expandableScrollViewDidEndScrollingAnimation?(scrollView)
+  }
+  
+  public func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+    return scrollViewDelegate?.viewForZoomingInExpandableScrollView?(scrollView)
+  }
+  
+  public func scrollViewWillBeginZooming(scrollView: UIScrollView, withView view: UIView?) {
+    scrollViewDelegate?.expandableScrollViewWillBeginZooming?(scrollView, withView: view)
+  }
+  
+  public func scrollViewDidEndZooming(scrollView: UIScrollView, withView view: UIView?, atScale scale: CGFloat) {
+    scrollViewDelegate?.expandableScrollViewDidEndZooming?(scrollView, withView: view, atScale: scale)
+  }
+  
+  public func scrollViewShouldScrollToTop(scrollView: UIScrollView) -> Bool {
+    return scrollViewDelegate?.expandableScrollViewShouldScrollToTop?(scrollView) ?? true
+  }
+  
+  public func scrollViewDidScrollToTop(scrollView: UIScrollView) {
+    scrollViewDelegate?.expandableScrollViewDidScrollToTop?(scrollView)
+  }
 }
